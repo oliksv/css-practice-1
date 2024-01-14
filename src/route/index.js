fe7905037ab6a7798d55725cbf92cef0567d8292
oliksv <oliksavkiv@gmail.com>
2024-01-14 20:08:42 +0200

    homework_slack

--- src/route/index.js
+++ src/route/index.js
@@ -1241,5 +1241,14 @@ router.get('/template-7', function (req, res) {
 })
 
 // ================================================================
+
+router.get('/slack', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('slack', {
+    layout: null,
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
